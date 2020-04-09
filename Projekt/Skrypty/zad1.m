e811bcec267f68ceb1f9069423ff7f5bf4ef62dd
grochu10 <mati.groszek9@interia.pl>
2020-04-09 13:17:00 +0200

    zad1 up

--- Projekt/Skrypty/zad1.m
+++ Projekt/Skrypty/zad1.m
@@ -16,26 +16,26 @@ F_ciep = 25;
 F_zimn = 40;
 F_zakl = 17;
 
-F_dop = F_ciep + F_zimn + F_zakl; %suma dop³ywów
+F = F_ciep + F_zimn + F_zakl; %suma dop³ywów
 
 %Temperatury doplywow
 T_ciep = 70;
 T_zimn = 30;
 T_zakl = 43;
 
-A = F_ciep*T_ciep + F_zmin*T_zimn + F_zakl*T_zakl;%rownanie doplywow
+A = F_ciep*T_ciep + F_zimn*T_zimn + F_zakl*T_zakl;%rownanie doplywow
 
 %Punkty poczatkowe
-h = (F_dop/alfa)^2;
+h = (F/alfa)^2;
 h0 = h;
 h_lin = h;
 
-T = A/F_dop;
+T = A/F;
 T0 = T;
 T_lin = T;
 
-V = C*h;
-V_lin = C*h_lin;
+V = C*h^2;
+V_lin = C*h_lin^2;
 
 %Okres probkowania
 
@@ -43,7 +43,7 @@ Tp = 1;
 
 %Skokowe zmiany sterowania
 
-%F_dop = F_dop - 30;
+F_dop = F +30;
 
 %Symulacja
 
@@ -72,25 +72,20 @@ for i = 1:n
     V_lin_pom = V_lin + 0.5*Tp*(F_dop - alfa*(sqrt(h0)+(1/(2*sqrt(h0))*(h_lin - h0))));
     h_lin_pom = sqrt(V_lin_pom/C);
     
-    dV_lin = Tp*(F_dop - alfa*sqrt(h_lin_pom));
+    dV_lin = Tp*(F_dop - alfa*(sqrt(h0)+(1/(2*sqrt(h0))*(h_lin_pom - h0))));
     V_lin = V_lin + dV_lin;
-    
-    %Temperatura cieczy w zbiorniku
-    
-    %%%%%%dopisac linearyzacje temperatury cieczy w zbiorniku%%%%%%
     
     %Dane do wykresu oraz aktualizacja danych
     
     V_vector(i) = V;
     V_lin_vector(i) = V_lin;
     
-    h = V/C;
-    h_lin = V_lin/C;
+    h = sqrt(V/C);
+    h_lin = sqrt(V_lin/C);
     h_vector(i) = h;
     h_lin_vector(i) = h_lin;
     
     T_vector(i) = T;
-    T_lin_vector(i) = T_lin;
     
 end
 
@@ -103,3 +98,22 @@ xlabel('t');
 ylabel('h');
 legend('h-zwykle','h-lin','Location','South')
 title('Wykres wysokoœci rzeczywistego oraz zlinearyzowanego poziomu cieczy')
+
+figure;
+plot(0:n-1,T_vector,'b')
+hold on
+xlabel('t');
+ylabel('T');
+legend('T-zwykle','Location','South')
+
+title('Wykres temperatury poziomu cieczy')
+figure;
+plot(h_lin_vector,V_lin_vector,'r')
+hold on
+plot([67.24 104.6013],[18084.8704 42442.1264],'g')
+%plot(h_vector,V_vector,'g')
+plot(h0,C*h0^2,'or')
+xlabel('h');
+ylabel('V');
+legend('V-lin','V-zwykle','Punkt pracy','Location','South')
+title('V(h)')
